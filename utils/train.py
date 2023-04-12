622fafca87d877a89717aeb09337afbadd885941
MaxMax2016 <525942103@qq.com>
2023-04-13 07:44:40 +0800

    pretrain config

--- utils/train.py
+++ utils/train.py
@@ -69,6 +69,13 @@ def train(rank, args, chkpt_path, hp, hp_str):
         logger = logging.getLogger()
         writer = MyWriter(hp, log_dir)
         valloader = create_dataloader(hp, False)
+    
+    if os.path.isfile(hp.train.pretrain):
+        if rank == 0:
+            logger.info("Start from pretrain model: %s" % hp.train.pretrain)
+        checkpoint = torch.load(hp.train.pretrain, map_location='cpu')
+        model_g.load_state_dict(checkpoint['model_g'])
+        model_d.load_state_dict(checkpoint['model_d'])
 
     if chkpt_path is not None:
         if rank == 0:
@@ -76,14 +83,15 @@ def train(rank, args, chkpt_path, hp, hp_str):
         checkpoint = torch.load(chkpt_path, map_location='cpu')
         model_g.load_state_dict(checkpoint['model_g'])
         model_d.load_state_dict(checkpoint['model_d'])
-        # optim_g.load_state_dict(checkpoint['optim_g'])
-        # optim_d.load_state_dict(checkpoint['optim_d'])
-        # step = checkpoint['step']
-        # init_epoch = checkpoint['epoch']
+        optim_g.load_state_dict(checkpoint['optim_g'])
+        optim_d.load_state_dict(checkpoint['optim_d'])
+        step = checkpoint['step']
+        init_epoch = checkpoint['epoch']
 
-        # if rank == 0:
-        #     if hp_str != checkpoint['hp_str']:
-        #         logger.warning("New hparams is different from checkpoint. Will use new.")
+        if rank == 0:
+            if hp_str != checkpoint['hp_str']:
+                logger.warning(
+                    "New hparams is different from checkpoint. Will use new.")
     else:
         if rank == 0:
             logger.info("Starting new training run.")
