f8203db1c956d79927bd9c572b4cb71d6d424a87
val <137660745+dcvalish@users.noreply.github.com>
2023-06-27 20:13:20 -0500

    Update easy_inference.py
    update

--- easy_inference.py
+++ easy_inference.py
@@ -15,7 +15,13 @@
 # You can also run the script and type -c in the file name and it will set it up for you
 #
 # Please avoid using any input files/model names with spaces / special characters as that will probably cause issues
-# Make sure what you call the model name matches the name you trained the model as in the chkpt/ folder when you set the name when you train and run this command: python svc_trainer.py -c config/maxgan.yaml -n MODEL_NAME
+# Make sure what you call the model name matches the name you trained the model as in the chkpt/ folder when you set the name when you train and run this command: python svc_trainer.py -c config/maxgan.yaml -s MODEL_NAME
+
+#Dev Notes / Plans 
+#add an ability to queue an entire folder of files through the ai
+#add a comma to run multiple files
+#add config option for convert_to_wav delete original files
+#try to implement a slicer by db to prevent cuda issues on long files?
 
 from pydub import AudioSegment
 import shutil
@@ -56,15 +62,11 @@ def remove_extension(file_name):
     base_name = os.path.splitext(file_name)[0]
     return base_name
 
-def replace_filename_in_commands(file_name, first_time, include_statics=False):
+def replace_filename_in_commands(file_name, include_statics=False):
     commands = [
         "python svc_inference_ppg.py -w WAV_INPUT.wav -p WAV_INPUT.ppg.npy",
         f"python svc_inference.py --config trained_models/{model_name}/maxgan.yaml --model trained_models/{model_name}/maxgan_g.pth --spk trained_models/{model_name}/lora_speaker.npy --ppg WAV_INPUT.ppg.npy --wave WAV_INPUT.wav"
     ]
-
-    if first_time:
-        current_dir = os.getcwd()
-        os.environ['PYTHONPATH'] = current_dir
 
     for command in commands:
         command = command.replace('WAV_INPUT', file_name)
@@ -89,7 +91,7 @@ def replace_filename_in_commands(file_name, first_time, include_statics=False):
     new_file_name = 'svc_{}_out_{}.wav'.format(model_name, file_name)
     if os.path.exists(svc_out_file):
 
-        # Add _depitched suffix to the renamed output file if -n flag is used
+        # Add _depitched suffix to the renamed output file if -s flag is used
         if include_statics:
             depitched_file_name = new_file_name.replace('.wav', '_depitched.wav')
             os.rename(svc_out_file, depitched_file_name)
@@ -97,167 +99,105 @@ def replace_filename_in_commands(file_name, first_time, include_statics=False):
         else:
             os.rename(svc_out_file, new_file_name)
             print("Renamed {} to {}".format(svc_out_file, new_file_name))
+    prompt_file()
     
-def process_file(file_name_input, first_time):
-    if " -n" in file_name_input:
-        file_name = file_name_input.replace(" -n", "")
-        if "." in file_name:
-            file_extension = os.path.splitext(file_name)[-1].lower()
-            if file_extension in supported_extensions:
-                convert_to_wav(file_name)
-                file_name = remove_extension(file_name)
-                replace_filename_in_commands(file_name, True, include_statics=True)
-            elif file_extension in [".wav"]:
-                file_name = remove_extension(file_name)
-                replace_filename_in_commands(file_name, True, include_statics=True)
-            else:
-                file_name = remove_extension(file_name)
-                print("Unsupported File Type")
-                file_name_input = input("Enter the file name: ")
-                process_file(file_name_input, False)
-        else:
-            # Check if any supported file variations exist in the folder
-            found_file = False
-
-            for extension in supported_extensions:
-                file_path = file_name + extension
-                if os.path.exists(file_path):
-                    convert_to_wav(file_path)
-                    file_name = remove_extension(file_path)
-                    found_file = True
-                    replace_filename_in_commands(file_name, True, include_statics=True)
-                    break
-
-            if not found_file:
-                # Check if WAV file exists
-                wav_file_path = file_name + ".wav"
-                if os.path.exists(wav_file_path):
-                    # Handle WAV file
-                    file_name = remove_extension(file_name)
-                    replace_filename_in_commands(file_name, True, include_statics=True)
-                else:
-                    print("Unsupported File Type")
-                    file_name_input = input("Enter the file name: ")
-                    process_file(file_name_input, False)
-    else:
-        file_name = file_name_input
-        if "." in file_name:
-            file_extension = os.path.splitext(file_name)[-1].lower()
-            if file_extension in supported_extensions:
-                convert_to_wav(file_name)
-                file_name = remove_extension(file_name)
-                replace_filename_in_commands(file_name, True, include_statics=False)
-            elif file_extension in [".wav"]:
-                file_name = remove_extension(file_name)
-                replace_filename_in_commands(file_name, True, include_statics=False)
-            else:
-                file_name = remove_extension(file_name)
-                print("Unsupported File Type")
-                file_name_input = input("Enter the file name: ")
-                process_file(file_name_input, False)
-        else:
-            # Check if any supported file variations exist in the folder
-            found_file = False
-            
-            for extension in supported_extensions:
-                file_path = file_name + extension
-                if os.path.exists(file_path):
-                    convert_to_wav(file_path)
-                    file_name = remove_extension(file_path)
-                    found_file = True
-                    replace_filename_in_commands(file_name, True, include_statics=False)
-                    break
-
-            if not found_file:
-                # Check if WAV file exists
-                wav_file_path = file_name + ".wav"
-                if os.path.exists(wav_file_path):
-                    # Handle WAV file
-                    file_name = remove_extension(file_name)
-                    replace_filename_in_commands(file_name, True, include_statics=False)
-                else:
-                    print("Unsupported File Type")
-                    file_name_input = input("Enter the file name: ")
-                    process_file(file_name_input, False)
 
 def cleanup_files():
     # Get the current script directory
+    cleanupConfirm = input(f"{name_prefix}Do you want to delete all input and output files or move and separate them into a raw/results/junkfiles folder to clean up the main directory? (delete/move/cancel): ")
+    
     script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # Define the paths to the folders
-    junk_files_dir = os.path.join(script_dir, "junkfiles")
-    results_dir = os.path.join(script_dir, "results")
-    raw_dir = os.path.join(script_dir, "raw")
-
-    # Create the folders if they don't exist
-    os.makedirs(junk_files_dir, exist_ok=True)
-    os.makedirs(results_dir, exist_ok=True)
-    os.makedirs(raw_dir, exist_ok=True)
 
     # Get a list of all files in the current directory
     files = os.listdir(script_dir)
 
-    # Iterate over the files
-    for file in files:
-        # Check if the file is a "ppg.npy" file
-        if file.endswith("ppg.npy"):
-            file_path = os.path.join(script_dir, file)
-            # Move the file to the "junkfiles" folder
-            shutil.move(file_path, os.path.join(junk_files_dir, file))
+    if cleanupConfirm == "delete":
+        # Iterate over the files
+        for file in files:
+            # Check if the file is a "ppg.npy" file
+            if file.endswith("ppg.npy"):
+                file_path = os.path.join(script_dir, file)
+                # Delete the file
+                os.remove(file_path)
 
-        # Check if the file is a WAV file containing "svc_" in its name
-        elif file.endswith(".wav") and "svc_" in file:
-            file_path = os.path.join(script_dir, file)
-            # Move the file to the "results" folder
-            shutil.move(file_path, os.path.join(results_dir, file))
+            # Check if the file is a WAV file containing "svc_" in its name
+            elif file.endswith(".wav") and "svc_" in file:
+                file_path = os.path.join(script_dir, file)
+                # Delete the file
+                os.remove(file_path)
 
-    # Get a list of remaining files in the current directory
-    files = os.listdir(script_dir)
+        print("All input and output files have been deleted.")
 
-    # Iterate over the remaining files
-    for file in files:
-        # Check if the file is a WAV, MP3, M4A, or FLAC file
-        if file.endswith((".wav",) + tuple(supported_extensions)):
-            file_path = os.path.join(script_dir, file)
-            # Move the file to the "raw" folder
-            shutil.move(file_path, os.path.join(raw_dir, file))
+    elif cleanupConfirm == "move":
+        # Define the paths to the folders
+        junk_files_dir = os.path.join(script_dir, "junkfiles")
+        results_dir = os.path.join(script_dir, "results")
+        raw_dir = os.path.join(script_dir, "raw")
 
-
-MODEL_NAME_FILE = "model_name.txt"
-
-supported_extensions = [".mp3", ".m4a", ".ogg", ".aac", ".mp4", ".mov", ".flac"]
-
-def save_model_name(model_name):
-    with open(MODEL_NAME_FILE, 'w') as file:
-        file.write(model_name)
-
-def load_model_name():
-    if os.path.exists(MODEL_NAME_FILE):
-        with open(MODEL_NAME_FILE, 'r') as file:
-            return file.read().strip()
-    return ""
-
-# Load the model name from the file if it exists
-model_name = load_model_name()
-# Process the initial file
-file_name_input = input("Enter the file name (-c for setup/config): ")
-
-if file_name_input == "-c":
-    valid_input = False
-    while not valid_input:
-        setuporconfig = input("Are you creating a setup or switching to a different model? (setup/switch/cleanup): ")
+        # Create the folders if they don't exist
+        os.makedirs(junk_files_dir, exist_ok=True)
+        os.makedirs(results_dir, exist_ok=True)
+        os.makedirs(raw_dir, exist_ok=True)
         
-        if setuporconfig == "setup":
+        # Iterate over the files
+        for file in files:
+            # Check if the file is a "ppg.npy" file
+            if file.endswith("ppg.npy"):
+                file_path = os.path.join(script_dir, file)
+                # Move the file to the "junkfiles" folder
+                shutil.move(file_path, os.path.join(junk_files_dir, file))
+
+            # Check if the file is a WAV file containing "svc_" in its name
+            elif file.endswith(".wav") and "svc_" in file:
+                file_path = os.path.join(script_dir, file)
+                # Move the file to the "results" folder
+                shutil.move(file_path, os.path.join(results_dir, file))
+
+        print("Input and output files have been moved to separate folders.")
+
+    elif cleanupConfirm == "cancel":
+        prompt_file()
+
+    else:
+        print("Invalid input. Please enter 'delete', 'move', or 'cancel'.")
+        cleanup_files()
+
+def configure():
+    global name_prefix
+    global model_name
+    configOption = input("Are you creating a setup or switching to a different model? (setup/switch/cleanup): ")
+    
+    if configOption == "setup":
+        validInput = False
+        while validInput == False:
             model_name = input("Enter your model name: ")
+            
+            # Check if the model directory exists
+            model_dir = os.path.join("chkpt", model_name)
+            if not os.path.exists(model_dir):
+                print("Model directory does not exist. Please enter a valid model name.")
+                print("[TIP] You can check your chkpt/ folder for what your model is called")
+                continue
+            
+            save_model_name(model_name)
+            name_prefix = "[" + model_name + "] "
             checkpoint_number = input("Enter checkpoint #: ")
             num_zeros = 8 - len(checkpoint_number)
             checkpoint_filename = f"{model_name}_{'0' * num_zeros}{checkpoint_number}.pt"
+            
+            # Check if the checkpoint file exists
+            checkpoint_path = os.path.join(model_dir, checkpoint_filename)
+            if not os.path.exists(checkpoint_path):
+                print("Checkpoint file does not exist. Please enter a valid checkpoint number.")
+                print("[TIP] For example if your checkpoint file is called mymodel_00012000.pt just enter 12000")
+                continue
             
             # Create trained_models/{model_name} directory if it doesn't exist
             trained_models_dir = "trained_models"
             model_dir = os.path.join(trained_models_dir, model_name)
             os.makedirs(model_dir, exist_ok=True)
             
+            # Export trained model
             command = f"python svc_inference_export.py --config config/maxgan.yaml --checkpoint_path chkpt/{model_name}/{checkpoint_filename}"
             os.system(command)
             
@@ -273,39 +213,119 @@ if file_name_input == "-c":
             
             # Copy data_svc/lora_pitch_statics.npy to trained_models/{model_name} folder
             shutil.copy("data_svc/lora_pitch_statics.npy", os.path.join(model_dir, "lora_pitch_statics.npy"))
+            prompt_file()
+            break
+
+        
+    elif configOption == "switch":
+        validInput = False
+        while validInput == False:
             
-            # Run the command python svc_inference_export.py --config config/maxgan.yaml --checkpoint_path chkpt/{model_name}/{checkpoint_filename}
-            valid_input = True
-            
-        elif setuporconfig == "switch":
             model_name = input("Enter your model name: ")
-            save_model_name(model_name)
-            process_file(file_name_input, True)
-            valid_input = True
-
-        elif setuporconfig == "cleanup":
-            cleanup_files()
-            print("Cleanup completed.")
-
-            valid_input = True
-        else:
-            print("Invalid Input. Please enter 'setup' or 'switch'.")
             
-        file_name_input = input("Enter the file name: ")
-        process_file(file_name_input, True)
-    
-else:
-    process_file(file_name_input, True)
+            model_dir = os.path.join("trained_models", model_name)
+            if not os.path.exists(model_dir):
+                print("Model directory does not exist. Please enter a valid model name.")
+                print("[TIP] You can check your trained_models/ folder for what your model is called")
+                continue
+            
+            name_prefix = "[" + model_name + "] "
+            save_model_name(model_name)
+            prompt_file()
+            break
 
-# Process more files
-while True:
-    answer = input("Do you want to process another file? (y/n): ")
-    if answer.lower() == 'n':
-        break
-    elif answer.lower() == 'y':
-        file_name_input = input("Enter the file name: ")
-
-        process_file(file_name_input, False)
+    elif configOption == "cleanup": 
+        cleanup_files()
+        print("Cleanup completed.")
+        prompt_file()
 
     else:
-        print("Invalid input. Please enter 'y' or 'n'.")
+        print("Invalid Input. Please enter 'setup', switch', or 'cleanup'.")
+        configure()
+
+def process_file(file_name, include_statics):
+    if "." in file_name:
+        file_extension = os.path.splitext(file_name)[-1].lower()
+        if file_extension in [".wav"]:
+            file_name = remove_extension(file_name)
+            replace_filename_in_commands(file_name, include_statics)
+        elif file_extension in SUPPORTED_EXTENSIONS:
+            convert_to_wav(file_name)
+            file_name = remove_extension(file_name)
+            replace_filename_in_commands(file_name, include_statics)
+        else:
+            file_name = remove_extension(file_name)
+            print("Error: File Not Found / Unsupported File Type")
+            prompt_file()
+    else:
+        # Check if any supported file variations exist in the folder
+        found_file = False
+        wav_file_path = file_name + ".wav"
+        if os.path.exists(wav_file_path):
+            # Handle WAV file
+            file_name = remove_extension(file_name)
+            replace_filename_in_commands(file_name, include_statics)
+        else:
+            # Check if any supported file variations exist in the folder
+            found_file = False
+
+            for extension in SUPPORTED_EXTENSIONS:
+                file_path = file_name + extension
+                if os.path.exists(file_path):
+                    convert_to_wav(file_path)
+                    file_name = remove_extension(file_path)
+                    found_file = True
+                    replace_filename_in_commands(file_name, include_statics)
+                    break
+
+            if not found_file:
+                print("Error: File Not Found / Unsupported File Type")
+                prompt_file()
+
+def prompt_file():
+    file_name_input = input(f"{name_prefix}Enter the file name: ")
+    if file_name_input == "-c":
+        configure()
+    elif file_name_input == "-e":
+        exit()
+    else:
+        if " -s" in file_name_input:
+            file_name = file_name_input.replace(" -s", "")
+            process_file(file_name, include_statics=True)
+        else:
+            file_name = file_name_input
+            process_file(file_name, include_statics=False)
+            
+
+def save_model_name(model_name):
+    with open(MODEL_NAME_FILE, 'w') as file:
+        file.write(model_name)
+        name_prefix = "[" + model_name + "] "
+
+
+def load_model_name():
+    if os.path.exists(MODEL_NAME_FILE):
+        with open(MODEL_NAME_FILE, 'r') as file:
+            return file.read().strip()
+    else:
+        return "MODEL_NOT_FOUND"
+    return ""
+
+# Loading in all the necessities 
+current_dir = os.getcwd()
+os.environ['PYTHONPATH'] = current_dir
+MODEL_NAME_FILE = "config.txt"
+SUPPORTED_EXTENSIONS = [".mp3", ".m4a", ".ogg", ".aac", ".mp4", ".mov", ".flac"]
+model_name = load_model_name()
+name_prefix = "[" + model_name + "] "
+
+# Information welcome screen
+print("\nLora Easy Inference:")
+print("\nAdding a -s to the end of the file name will inference with the statistics file")
+print("Using -s can sometimes make it sound off key so I don't recommend using it on singing inputs")
+print("Type in '-c' to enter the config to setup / switch to a different model")
+print("Type in '-e' to exit out of the program")
+print("If you see MODEL_NOT_FOUND that means you either deleted config.txt or you havent setup a model with -c yet")
+print("\nExample Inputs: -e, -c, test, test -s, test.wav, test.wav -s, test.mp3")
+
+prompt_file()
